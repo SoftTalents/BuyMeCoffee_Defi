32047a4dfba101922f67d458e687ee7a61cd050f
cryptohero92 <alwaysflexibleman@gmail.com>
2022-12-18 08:23:46 +0900

    frontend trial for buyMeCoffee contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,6 +5,7 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
   const BuyMeCoffee = await hre.ethers.getContractFactory("BuyMeCoffee");
@@ -15,6 +16,17 @@ async function main() {
   console.log(
     `BuyMeCoffee deployed to ${buyMeCoffee.address}`
   );
+
+  const data = {
+    address: buyMeCoffee.address,
+    abi: JSON.parse(buyMeCoffee.interface.format('json'))
+  }
+
+  const dir = './client/utils';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, {recursive: true});
+  }
+  fs.writeFileSync('./client/utils/BuyMeCoffee.json', JSON.stringify(data));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
