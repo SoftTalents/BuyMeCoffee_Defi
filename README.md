c853c267f96081928d98bde26747f5b417a5fbf8
alexpopov86 <alexanderpopov1986@outlook.com>
2022-12-19 13:57:18 +0900

    Polygon Mumbai sidechain added into hardhat setting

--- README.md
+++ README.md
@@ -17,6 +17,12 @@ Goerli deploy is below.
 ```shell
 npx hardhat run scripts/deploy.js --network goerli
 ```
+
+To deploy on polygon mumbai test net, run the following command.
+```shell
+npx hardhat run --network mumbai scripts/deploy.js --show-stack-traces
+```
+
 To see website, run the following command.
 ```shell
 cd client && npm run dev
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config()
 
 const GOERLI_API_KEY = process.env.GOERLI_API_KEY
+const MUMBAI_API_KEY = process.env.MUMBAI_API_KEY
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -10,7 +11,11 @@ module.exports = {
   networks: {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${GOERLI_API_KEY}`,
-      accounts: [PRIVATE_KEY]
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${MUMBAI_API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
   }
 };
